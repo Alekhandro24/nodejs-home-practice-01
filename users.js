00c0af8fbc3d2d1122f039ccfe806d90b5041133
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-02-13 23:42:31 +0200

    Create users.js

---
+++ users.js
@@ -0,0 +1,14 @@
+// const now = new Date();
+// console.log(now);
+
+const admins = ["Alex", "Andrey", "Vasiliy"];
+
+const clients = ["Anna ", "Alina", "Tamara"];
+
+const users = {
+  admins,
+  clients,
+};
+
+module.exports = users;
+// експорт за замовчанням
